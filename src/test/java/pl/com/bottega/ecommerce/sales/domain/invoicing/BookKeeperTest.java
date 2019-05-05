6598477ff2f635543d5408ca2f4d99ba8d2c9645
209278 <209278@edu.p.lodz.pl>
2019-05-05 18:27:14 +0200

    isolation - init and invoiceRequestWithOneProduct

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -0,0 +1,52 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class BookKeeperTest {
+    BookKeeper bookKeeper;
+
+    InvoiceFactory invoiceFactory;
+    InvoiceRequest invoiceRequest;
+    ClientData clientData;
+
+    TaxPolicy taxPolicy = mock(TaxPolicy.class);
+
+    ProductData productData = mock(ProductData.class);
+
+    @BeforeEach
+    public void init(){
+        clientData = new ClientData(Id.generate(), "name");
+        invoiceFactory = new InvoiceFactory();
+        invoiceRequest = new InvoiceRequest(clientData);
+
+        bookKeeper = new BookKeeper(invoiceFactory);
+    }
+
+    @Test
+    public void invoiceRequestWithOneProduct() {
+
+        Money money = new Money(1);
+
+        RequestItem requestItem = new RequestItem(productData, 1, money);
+        invoiceRequest.add(requestItem);
+
+        when(taxPolicy.calculateTax(any(ProductType.class), any(Money.class)))
+                .thenReturn(new Tax(new Money(1), "a"));
+
+        Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
+        MatcherAssert.assertThat(invoice.getItems().size(), Matchers.is(1));
+    }
+
+}
